5a8b8e9aa66ec27b9e8d548336ffa56460e9741a
Cory Sauve <csauve95@gmail.com>
2019-10-01 11:54:34 -0400

    adding org-N function

---
+++ R/find_orgn.R
@@ -0,0 +1,26 @@
+#' Organic Nitrogen
+#'
+#' Calculates the Organic-nitrogen concentration from total nitrogen, nitrate, and ammonia.
+#' @param tn_mgL Numeric value for total nitrogen concentration in milligrams per liter
+#' @param no3_mgL Numeric value for nitrate concentration in milligrams per liter
+#' @param nh3_mgL Numberic value for ammonia concentration in milligrams per liter
+#' @param location Character string to denote sampling location; only accepts for epilimnion -- "epi" or hypolimnion -- "hypo"
+#' @keywords nitrogen
+#' @export
+#' @examples
+#'
+#' find_orgn(0.50, 0.02, 0.01, "epi")
+
+find_orgn <- function(tn_mgL, no3_mgL, nh3_mgL, location){
+
+  if(location == "epi"){
+    organicN <- tn_mgL - (no3_mgL + nh3_mgL)
+    return(organicN)
+  } else if (location == "hypo") {
+    organicN <- tn_mgL - (no3_mgL + nh3_mgL)
+    return(organicN)
+  } else {
+    print("Did not supply correct location name")
+  }
+}
+
